a0777e3ea65a1512fce315020c2049de2d82289c
Hao Xiong <haxiong@microsoft.com>
2019-09-12 18:16:35 -0700

    test

--- client/main.go
+++ client/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/qiushuiqs/idhash-benchmark/cache"
 	"github.com/qiushuiqs/idhash-benchmark/connection"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("40.91.88.191:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
 
 	s := "CgObKRQxNTQ3MzQzMTc4MDU1NzM4MjU5MAAA"
 	numOfError := 0
@@ -25,6 +35,17 @@ func main() {
 		}()
 	}
 
+	// s := "CgObKRQxNTQ3MzQzMTc4MDU1NzM4MjU5MAAA"
+	sDec, _ := base64.StdEncoding.DecodeString("CgObKRQxNTQ3MzQzMTc4MDU1NzM4MjU5MAAA")
+	req := &cache.Request{
+		MUID: string(sDec),
+	}
+	c := cache.NewCacheServiceClient(conn)
+	response, err := c.ProcessRequest(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Println(response.String())
 }
 
 func timeTrack(start time.Time, name string) {
